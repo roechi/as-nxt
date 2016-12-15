0ccb797fc2af66760d3ae5c51deaef7a9fa3bf22
roechi <msg.remus@gmail.com>
2016-12-15 13:56:29 +0100

    starts refactoring

---
+++ explorer.h
@@ -0,0 +1,17 @@
+void initMoveSpace(char* environmnet, short x_dim, short y_dim);
+void doStuff(char &environment[]);
+void see(char &environment[], short &directions, short length);
+bool isOccupied();
+short backOff();
+short findNewDirection();
+void rotateOnSpot(int direction, float wheelDistance, float wheelRadius, int angle);
+void moveForward(long speed);
+void backForward(long speed);
+void stopMove();
+void rotate90(int direction);
+void turnRight();
+void turnLeft();
+void turnAround();
+bool checkGround();
+void initGroundDetectors();
+void drive(int distance, int direction);
