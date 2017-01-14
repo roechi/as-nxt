3b652995d0e4d830427877e288f54920562a2805
git <flo4s@gmx.net>
2017-01-14 13:41:58 +0100

    branch added

---
+++ current_State.h
@@ -0,0 +1,5 @@
+struct Current_State {
+	char pos_x;
+	char pos_y;
+	char direction;
+};
---
+++ explorer.h
@@ -0,0 +1,30 @@
+// Initialization
+void init_MoveSpace();
+void init_current_state(char x, char y, char direction);
+void init_ground_detectors();
+
+void doStuff();
+bool see();
+
+void update_current_position(char direction);
+void reorient();
+char get_field_in_direction(char direction);
+bool can_move(short direction);
+void move();
+char find_new_direction();
+bool is_occupied();
+bool check_ground();
+
+// Movement
+void rotateOnSpot(char direction, float wheelDistance, float wheelRadius, int angle);
+void moveForward(char speed);
+void moveBackward(char speed);
+void backOff();
+void stopMove();
+void rotate90(char direction);
+void turnRight();
+void turnLeft();
+void turnAround();
+void drive(int distance, int direction);
+
+
