7c20dd6f1503b424b4da51a0dbcb649b92d27f35
git <flo4s@gmx.net>
2017-01-14 11:47:43 +0100

    some refactoring

--- explorer.h
+++ explorer.h
@@ -1,18 +1,38 @@
-void initMoveSpace();
+// Initialization
+void init_MoveSpace();
+void init_current_state(char x, char y, char direction);
+void init_ground_detectors();
+
 void doStuff();
-void see(short &directions[], short directions_length);
+void see();
+
+void update_current_position(char direction);
+void reorient();
+char get_field_in_direction(char direction);
+bool can_move(short direction);
+void move()
 bool isOccupied();
-short backOff();
-short findNewDirection();
-void rotateOnSpot(int direction, float wheelDistance, float wheelRadius, int angle);
-void moveForward(long speed);
-void backForward(long speed);
+
+
+
+
+// Movement
+void rotateOnSpot(char direction, float wheelDistance, float wheelRadius, int angle);
+void moveForward(char speed);
+void moveBackward(char speed);
+void backOff();
 void stopMove();
-void rotate90(int direction);
+void rotate90(char direction);
 void turnRight();
 void turnLeft();
 void turnAround();
-bool checkGround();
-void initGroundDetectors();
 void drive(int distance, int direction);
-void move();
+
+
+char find_new_direction();
+
+bool checkGround();
+
+
+
+
